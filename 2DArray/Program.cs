f27e89e41f428e4f97d0d967972166ad5d4dca73
Malik <mlk.2plus@gmail.com>
2022-07-19 23:13:44 +0200

    First commit

---
+++ 2DArray/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+int[][]array2D = new int [][]  // Array of Arrays C# OR Jagged Arrays  C#
+{
+	new int[] {1,2,3,4},
+	new int[] {5,6,7,8},
+	new int[] {9,10,11,12},
+}; 
+
+
+int wealth  = 0;
+int richest = 0;
+
+  int MaximumWealth(int [][] accounts) {
+        
+    for (int i = 0; i < accounts.Length; i++) // n
+        {
+	        for (int j = 0; j < accounts[0].Length; j++) // n
+	            {
+		            wealth  += accounts[i][j];
+	            }
+
+	             if (wealth  > richest)
+	                {
+		                richest = wealth ;
+	                }
+
+	                wealth = 0;
+        }
+        
+        return richest;
+    }
+
+Console.WriteLine(MaximumWealth(array2D));
+
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("Big o (Time Complexity) of this is O(n^2)");
+Console.WriteLine("-----------------------------------------------------");
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List <string> Output = new List <string> ();
+
+IList<string> FizzBuzz(int n) 
+{
+
+	for (int i = 1; i <= n; i++)   // n
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			   {
+					Output.Add("FizzBuzz");
+			   }
+			else if (i % 3 == 0)
+					{
+						Output.Add("Fizz");
+					}
+			else if (i % 5 == 0)
+					{
+						Output.Add("Buzz");
+					}
+			else 
+				{
+					Output.Add(i.ToString());
+				}
+		}
+
+		return (Output);
+}
+
+
+
+foreach (string item in FizzBuzz(5))
+{
+	Console.WriteLine(item);
+}
+
+Console.WriteLine ("----------------------------------------------------");
+Console.WriteLine ("Big o (Time Complexity) of this is O(n)");	
+Console.WriteLine ("----------------------------------------------------");
