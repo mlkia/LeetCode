13717232ad4e4b84ce9af9a41cfacc860627e1cd
Malik <mlk.2plus@gmail.com>
2022-07-21 21:17:35 +0200

    push NumberOfSteps

---
+++ NumberOfSteps/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//1342. Number of Steps to Reduce a Number to Zero
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+int steps = 0;
+
+int NumberOfSteps(int num) 
+ {
+
+	while (num > 0)
+	{
+		if (num % 2 == 0)
+		{
+			num = num / 2;
+		}
+		else 
+		{
+			num -=1;
+		}
+
+		steps +=1;
+	}
+	
+	return steps;
+ }
+
+ Console.WriteLine(NumberOfSteps(14) + " steps");
+
+Console.WriteLine ("----------------------------------------------------");
+Console.WriteLine ("Big o (Time Complexity) of this is O(n)");	
+Console.WriteLine ("----------------------------------------------------");
+
+//Output: 6 steps
+
